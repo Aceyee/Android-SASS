9b96d2829b68368beebc2d3cc73daea9d39130cd
Zihan Ye <zaeye1028@gmail.com>
2018-06-20 08:29:23 -0700

    code optimized

--- app/src/main/java/com/example/zihan/sss/BackgroundTask.java
+++ app/src/main/java/com/example/zihan/sss/BackgroundTask.java
@@ -36,74 +36,83 @@ public class BackgroundTask extends AsyncTask<String, Void, String> {
 
     @Override
     protected String doInBackground(String... params) {
-        /* According to Emulator Networking IP 10.0.2.2 should be used instead of localhost/127.0.0.1. */
-        String reg_url = "http://10.0.2.2/register.php";
-        String login_url = "http://10.0.2.2/login.php";
         String method =params[0];
         if(method.equals("register")){
-            String user_name = params[1];
-            String user_pass = params[2];
-            String user_roll = params[3];
-            try {
-                URL url = new URL(reg_url);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoOutput(true);
-                //httpURLConnection.setDoInput(true);
-                OutputStream OS = httpURLConnection.getOutputStream();
-                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(OS, "UTF-8"));
-                String data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"+
-                        URLEncoder.encode("user_pass","UTF-8")+"="+URLEncoder.encode(user_pass,"UTF-8")+"&"+
-                        URLEncoder.encode("user_roll","UTF-8")+"="+URLEncoder.encode(user_roll,"UTF-8");
-                bufferedWriter.write(data);
-                bufferedWriter.flush();
-                bufferedWriter.close();
-                OS.close();
-                InputStream IS = httpURLConnection.getInputStream();
-                IS.close();
-                return "Registration Success";
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-                return "Debug: fail 1";
-            } catch (IOException e) {
-                e.printStackTrace();
-                return "Debug: fail 2";
-            }
+            return BackgroundRegister(params);
         }else if(method.equals("login")){
-            String login_name = params[1];
-            String login_pass = params[2];
-            try {
-                URL url = new URL(login_url);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoOutput(true);
-                httpURLConnection.setDoInput(true);
-                OutputStream outputStream = httpURLConnection.getOutputStream();
-                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-                String data = URLEncoder.encode("login_name", "UTF-8")+"="+URLEncoder.encode(login_name,"UTF-8")+"&"+
-                        URLEncoder.encode("login_pass", "UTF-8")+"="+URLEncoder.encode(login_pass,"UTF-8");
-                bufferedWriter.write(data);
-                bufferedWriter.flush();
-                bufferedWriter.close();
-                outputStream.close();
-
-                InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-                String response = "";
-                String line ="";
-                while ((line=bufferedReader.readLine())!=null){
-                    response +=line;
-                }
-                bufferedReader.close();
-                inputStream.close();
-                return response;
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            return BackgroundLogin(params);
         }
-        return "registration failed";
+        return "register/login fail";
+    }
+
+    private String BackgroundLogin(String[] params) {
+        String login_url = "http://10.0.2.2/login.php";
+        String login_name = params[1];
+        String login_pass = params[2];
+        try {
+            URL url = new URL(login_url);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setDoInput(true);
+            OutputStream outputStream = httpURLConnection.getOutputStream();
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+            String data = URLEncoder.encode("login_name", "UTF-8")+"="+URLEncoder.encode(login_name,"UTF-8")+"&"+
+                    URLEncoder.encode("login_pass", "UTF-8")+"="+URLEncoder.encode(login_pass,"UTF-8");
+            bufferedWriter.write(data);
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            outputStream.close();
+
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            String response = "";
+            String line ="";
+            while ((line=bufferedReader.readLine())!=null){
+                response +=line;
+            }
+            bufferedReader.close();
+            inputStream.close();
+            return response;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Login fail";
+    }
+
+    private String BackgroundRegister(String[] params) {
+        /* According to Emulator Networking IP 10.0.2.2 should be used instead of localhost/127.0.0.1. */
+        String reg_url = "http://10.0.2.2/register.php";
+        String user_name = params[1];
+        String user_pass = params[2];
+        String user_roll = params[3];
+        try {
+            URL url = new URL(reg_url);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoOutput(true);
+            //httpURLConnection.setDoInput(true);
+            OutputStream OS = httpURLConnection.getOutputStream();
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(OS, "UTF-8"));
+            String data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"+
+                    URLEncoder.encode("user_pass","UTF-8")+"="+URLEncoder.encode(user_pass,"UTF-8")+"&"+
+                    URLEncoder.encode("user_roll","UTF-8")+"="+URLEncoder.encode(user_roll,"UTF-8");
+            bufferedWriter.write(data);
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            OS.close();
+            InputStream IS = httpURLConnection.getInputStream();
+            IS.close();
+            return "Registration Success";
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return "Debug: fail 1";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Debug: fail 2";
+        }
     }
 
     @Override
